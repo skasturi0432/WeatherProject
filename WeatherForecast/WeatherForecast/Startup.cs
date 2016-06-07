6e15d4893702840ff6cf8b88936582f9431c8fc6
Miniyahil Arekew <Miniyahil.Arekew.Contractor@hilton.com>
2016-06-07 16:24:50 -0500

    Initial skeleton of default MVC Project

---
+++ WeatherForecast/WeatherForecast/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeatherForecast.Startup))]
+namespace WeatherForecast
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
